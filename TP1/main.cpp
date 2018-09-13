d2d127a3ef55560063c13762f398d7f264d9821e
itspck <mun.benoit@gmail.com>
2018-09-13 09:48:41 +0200

    Q1 et Q2

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,11 +1,43 @@
+/* Q1
+
 #include <iostream>
 using namespace std;
 
-/*Fonction principale*/
+
 int main(int argc, char ** argv)
 {
   for(int i=0; i< 120; ++i)
     cout << "Bonjour les ZZ" << 2 << endl;
+
+  return 0;
+}
+
+*/
+
+/*
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  for(int i=0; i< 120; ++i)
+    std::cout << "Bonjour les ZZ" << 2 << std::endl;
+
+  return 0;
+}*/
+
+#include <iostream>
+#include <string>
+
+int main(int, char **)
+{
+  std::string prenom; // type spécial pour les chaînes de caractères
+  int age;
+
+  std::cout << "Quel est votre prénom ?" << std::endl;
+  std::cin >> prenom;
+  std::cout << "Quel est votre age ?" << std::endl;
+  std::cin >> age ;
+  std::cout << "Bonjour "<< prenom << std::endl;
 
   return 0;
 }
