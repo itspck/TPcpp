ec30e50892c7a0e4001c2d95c9b2e3e44d40ca0b
itspck <mun.benoit@gmail.com>
2018-09-13 11:32:59 +0200

    Testing the instanciation

--- TP2/Bavarde.cpp
+++ TP2/Bavarde.cpp
@@ -1,1 +1,36 @@
 #include <iostream>
+using namespace std;
+
+
+class Bavarde {
+  int v;
+  public :
+	Bavarde(){
+		cout << "sans args" << endl;
+	}
+	
+	Bavarde(int i){
+		v = i;
+		cout << "avec args" << endl;
+	}
+	
+	~Bavarde(){
+		cout << "Tais-toi" << endl;
+	}
+} bizarre(1);
+
+//Bavarde globale(2);
+
+void fonction(Bavarde &b) {
+	cout << "code de la fonction" << endl;
+}
+
+int main(int, char **)
+{
+	Bavarde b;
+	Bavarde * p = new Bavarde(3);
+	fonction(b);
+	delete p;
+  return 0;
+}
+
